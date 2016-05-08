a6408026a095417360ab891ecf03d1e6b7db4ae7
breus32 <muller.09@mail.ru>
2016-05-08 08:42:23 +0300

    Цілі від 0 до n

--- 1.a(arrays.md).c
+++ 1.a(arrays.md).c
@@ -12,24 +12,21 @@ int get_random_int_min_max(int min_rand, int max_rand)
     return min_rand + rand() % (max_rand - min_rand);
 }
 
-
-main()
+int main()
 {
     int mas[100];
     int n, i;
     int max_rand;
 	
-	printf("Vvedite n= ");
-	scanf("%d", &n);
-	
+    printf("Vvedite n= ");
+    scanf("%d", &n);
 
     srand(time(0));
 
     for (i = 0; i < n; i++) {
 	mas[i] = get_random_int_min_max(0, n);
+	printf("%d\n", mas[i]);
     }
-
-    for (i = 0; i < n; i++) {
-        printf("%d\n", mas[i]);
-    }
+    
+    return 0;
 }
