499d328baab36ca51374da8be5a561373f3e65b4
breus32 <muller.09@mail.ru>
2016-05-08 09:14:59 +0300

    Послідовність від -10 до 10 з кроком 0.1

--- 2.d(arrays.md).c
+++ 2.d(arrays.md).c
@@ -3,11 +3,11 @@
 int main ()
 {
 	int i;
-	double mas[100];
+	double mas[300];
 	double a;
 
-	
-	for(i = 0, a = -10; a < 10; a = a + 0.1, i++) {
+	for(i = 0, a = -10; a < 10; i++) {
+		a = a + 0.1;
 		mas[i] = a;
 		printf("%.1lf\n", a);
 	}	
