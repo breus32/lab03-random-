c21bee74cf6d565c07bfca7eb71aa2e001cda353
breus32 <muller.09@mail.ru>
2016-05-19 20:31:14 +0300

    Дійсні від 0 до n

--- 1.d(arrays.md).c
+++ 1.d(arrays.md).c
@@ -1,32 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
-int get_random_int(int max_rand)
-{
-    return rand() % max_rand;
-}
-
-
-int get_random_int_min_max(int min_rand, int max_rand)
-{
-    return min_rand + rand() % (max_rand - min_rand);
-}
 
 int main() 
 {
-	double mas[100], n;
-	int i;
+	double mas[300], n;
+	int i, size;
 	
-	printf("Vvedite n:");
+	printf("Enter array size: ");
+	scanf("%d", &size);
+	
+	printf("\nEnter n: ");
 	scanf("%lf", &n);
 	
 	srand(time(0));
 	
-	for(i = 0; i < n; i++) {
-		mas[i] = get_random_int_min_max(0, n);
-		printf("\nmas[%d] = %lf", i, mas[i]);
+	for(i = 0; i < size; i++) {
+		mas[i] = ((float)rand() / RAND_MAX) * n;
+		printf("\n\tmas[%d] = %lf", i, mas[i]);
 	}
 	
 	return 0;
 }
-
